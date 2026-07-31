09b722e6a70bfc77b8339c681c3cb0f7b14154d1
corpus-delicti-0 <zalev.j@gmail.com>
2026-07-31 20:35:20 -0300

    implemented a help and exit functions

--- repl.go
+++ repl.go
@@ -7,6 +7,25 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var registry = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+}
+
 func cleanInput(text string) []string {
 	result := []string{}
 	for part := range strings.SplitSeq(text, " ") {
@@ -27,8 +46,17 @@ func runREPL() error {
 		}
 		input := scanner.Text()
 		result := cleanInput(input)
-		if len(result) != 0 {
-			fmt.Printf("Your command was: %s\n", result[0])
+		if len(result) == 0 {
+			continue
+		}
+		command, ok := registry[result[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+		} else {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
